8db431dcdde1a3c101f94ffb05a5a00c54eca9b3
Gabriel Kim <gabrielkim13@gmail.com>
2020-07-27 23:12:37 -0300

    Finish Home page by adding provided components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 
+import data from "./data/dados_iniciais.json";
+
 import Menu from "./components/Menu";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <div>
+        <div style={{ background: "#141414" }}>
             <Menu />
+
+            <BannerMain
+                videoTitle={data.categorias[0].videos[0].titulo}
+                videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa! "
+                url={data.categorias[0].videos[0].url}
+            />
+
+            {data.categorias.map((category, index) => {
+                if (index === 0)
+                    return <Carousel ignoreFirstVideo category={category} />;
+
+                return <Carousel category={category} />;
+            })}
+
+            <Footer />
         </div>
     );
 }
