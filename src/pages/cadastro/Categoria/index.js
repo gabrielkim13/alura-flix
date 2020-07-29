4108bb08a472c293594a9f33598f8dfea1ad34b6
Gabriel Kim <gabrielkim13@gmail.com>
2020-07-28 22:43:27 -0300

    Infinite Carousel and basic form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -8,6 +8,15 @@ function CadastroCategoria() {
         <PageDefault>
             <h1>New category</h1>
 
+            <form>
+                <label>
+                    Name:
+                    <input type="text" />
+                </label>
+
+                <button>Add category</button>
+            </form>
+
             <Link to="/">Home</Link>
         </PageDefault>
     );
